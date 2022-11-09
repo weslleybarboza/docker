c846dd998aa29609163dff017ad90f20cc54290d
weslleybarboza <weslleybarboza@gmail.com>
2022-11-09 09:28:50 +0100

    exercicios docker

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add new item to list'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
